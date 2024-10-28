1be4626d4fb404018d4ff19ff65fcb692acaeb50
Doğukan Aydoğdu <daydogdu93@gmail.com>
2024-10-28 17:34:38 +0100

    nvim tree added

--- init.lua
+++ init.lua
@@ -22,7 +22,19 @@ require('packer').startup(function(use)
             end)
         end,
     }
-
+    -- Nvim-Tree
+    use {
+        "nvim-tree/nvim-tree.lua",
+        requires = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require("nvim-tree").setup({
+                sort_by = "case_sensitive",
+                filters = {
+                    dotfiles = true,
+                },
+            })
+        end,
+    }
     -- Harpoon
     use {
         'ThePrimeagen/harpoon',
@@ -35,10 +47,10 @@ require('packer').startup(function(use)
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+            -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
@@ -140,7 +152,57 @@ vim.cmd([[colorscheme gruvbox]])
 -- Additional Key Mappings
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+-- local function my_on_attach(bufnr)
+--   local api = require "nvim-tree.api"
+--
+--   local function opts(desc)
+--     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+--   end
+--
+--   -- default mappings
+--   api.config.mappings.default_on_attach(bufnr)
+--
+--   -- custom mappings
+--   vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+--   vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+-- end
+--
+-- -- pass to setup along with your other options
+-- require("nvim-tree").setup {
+--   ---
+--   on_attach = my_on_attach,
+--   ---
+-- }
+
+
+vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>f', ':NvimTreeFindFile!<CR>', { noremap = true })
 -- LSP-Specific Key Mappings
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
